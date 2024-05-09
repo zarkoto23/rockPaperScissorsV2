a762a88dc5066649956c2fa5871395bf48bf60fc
zarkoto23 <162752996+zarkoto23@users.noreply.github.com>
2024-05-09 10:30:37 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-const computerChoices=['Камък', 'Ножица', 'Хартия']
-const computerChoice=document.getElementById('computer')
-const playerChoice=document.getElementById('player')
-const winnerChek=document.getElementById('winner')
+const computerChoices = ["Камък", "Ножица", "Хартия"];
+const computerChoice = document.getElementById("computer");
+const playerChoice = document.getElementById("player");
+const winnerChek = document.getElementById("winner");
 
+const playerScoreDisplay = document.getElementById("playerPoints");
+const coputerScoreDisplay = document.getElementById("computerPoints");
+const tieScoreDisplay = document.getElementById("tiePoints");
 
-let  computerTurn
-let playerTurn
-let result=''
+let playerCounter = 0;
+let computerCounter = 0;
+let tieCounter = 0;
 
+let computerTurn;
+let playerTurn;
+let result = "";
 
+function playLogic(playerTurn) {
+  computerTurn = computerChoices[Math.floor(Math.random() * 3)];
 
-function playLogic(playerTurn){
- computerTurn=computerChoices[Math.floor(Math.random()*3)]
-  
-  
-  if(playerTurn===computerTurn){
-    result= 'Равенство!'
-  }else if(playerTurn==='Камък'){
-    result= (computerTurn==='Ножица') ? 'Победа !':'Загуба !'
-  }else if(playerTurn==='Ножица'){
-    result= (computerTurn==='Хартия')? 'Победа !':'Загуба !'
-  }else if(playerTurn==='Хартия'){
-    result=( computerTurn==='Камък')? 'Победа !' : 'Загуба !'
+  if (playerTurn === computerTurn) {
+    result = "Равенство!";
+    tieCounter++;
+    tieScoreDisplay.textContent = tieCounter;
+  } else if (playerTurn === "Камък") {
+    result = computerTurn === "Ножица" ? "Победа !" : "Загуба !";
+  } else if (playerTurn === "Ножица") {
+    result = computerTurn === "Хартия" ? "Победа !" : "Загуба !";
+  } else if (playerTurn === "Хартия") {
+    result = computerTurn === "Камък" ? "Победа !" : "Загуба !";
   }
 
-  playerChoice.textContent=`Играч: ${playerTurn}`
-  computerChoice.textContent=`Компютър: ${computerTurn}`
-  winnerChek.textContent=result
+  playerChoice.textContent = `Играч: ${playerTurn}`;
+  computerChoice.textContent = `Компютър: ${computerTurn}`;
+  winnerChek.textContent = result;
 
+  winnerChek.classList.remove("greenTxt", "redTxt");
 
+  switch (result) {
+    case "Победа !":
+      winnerChek.classList.add("greenTxt");
+      playerCounter++;
+      playerScoreDisplay.textContent = playerCounter;
+      break;
+    case "Загуба !":
+      winnerChek.classList.add("redTxt");
+      computerCounter++;
+      coputerScoreDisplay.textContent = computerCounter;
+      break;
+  }
 }
-
-
-
-
-
